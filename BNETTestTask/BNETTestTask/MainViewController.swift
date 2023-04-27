0ef1227c1a9e35023966868ef47f32c876f53773
Alexey Statiev <31804144+CT4TuEI3@users.noreply.github.com>
2023-04-27 21:31:22 +0300

    Add files via upload

---
+++ BNETTestTask/BNETTestTask/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  BNETTestTask
+//
+//  Created by Алексей on 27.04.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
